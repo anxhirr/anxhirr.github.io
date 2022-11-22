e0949c81bf09f547534084dd2f99d9e693c9ea4d
anxhirr <anxhirr@gmail.com>
2022-11-22 22:07:06 +0100

    another attempt to fix gh pages SPA routing

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/anxhirr.github.io'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
