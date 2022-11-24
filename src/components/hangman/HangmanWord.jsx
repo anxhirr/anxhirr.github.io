54c77f9da88f0c674f864861add876ae28e0327f
anxhirr <anxhirr@gmail.com>
2022-11-24 12:09:21 +0100

    fixed some mobile responsiveness on hangman-game

--- src/components/hangman/HangmanWord.jsx
+++ src/components/hangman/HangmanWord.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const HangmanWord = ({ toGuessWord, guessedLetters, hasWon }) => {
+const HangmanWord = ({ toGuessWord, guessedLetters, hasLost, hasWon }) => {
   return (
     <div className='hangman__word'>
       {toGuessWord.split('').map((letter, index) => (
@@ -8,9 +8,14 @@ const HangmanWord = ({ toGuessWord, guessedLetters, hasWon }) => {
           <span
             key={index}
             className={`${
-              guessedLetters.includes(letter)
+              guessedLetters.includes(letter) || hasLost
                 ? 'hangman__letter--visible'
                 : 'hangman__letter--hidden'
+            }
+            ${
+              !guessedLetters.includes(letter) && hasLost
+                ? 'hangman__letter--red'
+                : 'hangman__letter--black'
             }
                 ${hasWon ? 'hangman__letter--winner' : ''}
               `}
